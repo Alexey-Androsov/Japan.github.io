e7dd79f2ee78bcbcf7b1c58ecc8570db5f7421d2
Aleksei Androsov <73528874+Alexey-Androsov@users.noreply.github.com>
2022-11-27 19:17:06 +0700

    Add files via upload

---
+++ term.js
@@ -0,0 +1,17 @@
+/*function calculate(arr){
+    let calculate = 0;
+    for (let i = 0; i < arr.Length; i++){
+        if (arr[i] > 0 && arr[i] % 2 !== 0) {
+            calculate += arr[i];
+        }
+    }
+    return calculate;
+}
+
+
+
+
+
+
+console.log(calculate([5, 0, -5, 20, 88, 17, -32]))//22*/
+console.log('efrg');
